44a31189e45ce16a8826feb118c87e8be5368a2b
shadowctrl <shadowctrl@shadowctrl.com>
2025-02-11 00:29:01 +0530

    Add Google Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <GoogleAnalytics gaId="G-L7P1STX8NR" />
       </body>
     </html>
   );
