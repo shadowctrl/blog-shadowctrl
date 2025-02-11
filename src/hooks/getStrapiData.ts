e7347204440789204a2ffcface3b79fbf878c586
shadowctrl <shadowctrl@shadowctrl.com>
2025-02-11 15:15:36 +0530

    `Refactor getStrapiData and getCategories functions with error handling and code organization`

--- src/hooks/getStrapiData.ts
+++ src/hooks/getStrapiData.ts
@@ -3,44 +3,52 @@ import { StrapiDataType } from "@/types";
 export const getStrapiData = async (
   slug?: string | undefined
 ): Promise<StrapiDataType> => {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_STRAPI_ENDPOINT +
-      "/api/articles?populate=cover&populate=author&populate=category",
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
-      },
+  try {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_STRAPI_ENDPOINT +
+        "/api/articles?populate=cover&populate=author&populate=category",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
+        },
+      }
+    );
+    const data = await response.json();
+
+    if (slug) {
+      const article = data.data.find((article: any) => article.slug === slug);
+      if (!article) return { status: 404, data: article };
+
+      return { status: 200, data: article };
     }
-  );
-  const data = await response.json();
 
-  if (slug) {
-    const article = data.data.find((article: any) => article.slug === slug);
-    if (!article) return { status: 404, data: article };
-
-    return { status: 200, data: article };
+    return { status: 200, data };
+  } catch {
+    return { status: 500, data: null };
   }
-
-  return { status: 200, data };
 };
 
 export const getCategories = async (): Promise<StrapiDataType> => {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_STRAPI_ENDPOINT +
-      "/api/categories?populate=articles",
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
-      },
-    }
-  );
-  if (!response.ok) return { status: 500, data: null };
-
   try {
-    const data = await response.json();
-    return { status: 200, data };
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_STRAPI_ENDPOINT +
+        "/api/categories?populate=articles",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_TOKEN}`,
+        },
+      }
+    );
+    if (!response.ok) return { status: 500, data: null };
+
+    try {
+      const data = await response.json();
+      return { status: 200, data };
+    } catch {
+      return { status: 500, data: null };
+    }
   } catch {
     return { status: 500, data: null };
   }
